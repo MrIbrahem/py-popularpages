ad48d98ab5cb502f49831f3a690ee9e2c34092b2
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-28 07:17:00 +0300

    .

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 # toolforge-jobs run test --image python3.11 --command "~/REPOS_2026/py-popularpages/test.sh"
 cd ~/REPOS_2026/py-popularpages
 
+pip install -r requirements.txt
+
 python3 src/cli/generate_report.py --wiki en.wikipedia --project Dinosaurs --dry-run
